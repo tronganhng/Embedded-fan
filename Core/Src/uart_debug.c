b37581ef321b67b4d4636917f5eca048a80f2833
Nguyễn Hữu Trọng Anh - GX2 NextGen <anhnht@rocketstudio.com.vn>
2025-06-17 21:39:24 +0700

    giải thích timer

--- Core/Src/uart_debug.c
+++ Core/Src/uart_debug.c
@@ -1,21 +1,22 @@
-//#include "uart_debug.h"
-//
-//void uart_init(void) {
+// #include "uart_debug.h"
+
+// void uart_init(void) {
 //    // APB2ENR : điều khiển các phần đc cấp xung clock
 //    // bật các bit xung clock cho: uart, gpioa, afi(hỗ trợ chức năng ngoài cho các chân gpio)
 //    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_USART1EN | RCC_APB2ENR_AFIOEN;
-//
+
 //    // CRH = [PA15][PA14]...[PA9][PA8] (4 bit mỗi chân) -> 32 bit
+//    // CHR : Control Reg High
 //    GPIOA->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9); // phần PA9 trong CRH = 0000
-//    GPIOA->CRH |= (0x0B << 4); // set mode PA9: TX = AF push-pull 50MHz
-//
+//    GPIOA->CRH |= (0x0B << 4); // set mode PA9 = 1011: TX = AF, 10: push-pull, 11: Output và 50MHz
+
 //    USART1->BRR = 72000000 / 9600; // tốc độ truyền data cho UART
 //    //UE - USART Enable (cho phép USART hoạt động)
 //    //TE – Transmitter Enable (cho phép truyền dữ liệu)
 //    USART1->CR1 = USART_CR1_TE | USART_CR1_UE;
-//}
-//
-//void uart_send(char *s) {
+// }
+
+// void uart_send(char *s) {
 //    while (*s) {
 //        // USART1->SR & USART_SR_TXE = 1 -> while(false) -> thoát khỏi while
 //        // phần TXE của SR = 1 khi có thể ghi dữ liệu mới vào DR
@@ -24,4 +25,4 @@
 //        while (!(USART1->SR & USART_SR_TXE));
 //        USART1->DR = *s++;
 //    }
-//}
+// }
